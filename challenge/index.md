d93169f8d4422cb87873f863b25f3b3b4b15266a
Shirley DeCesari <shirley.decesari@gmail.com>
2022-03-01 21:43:25 -0800

    Add annotations

--- challenge/index.md
+++ challenge/index.md
@@ -1,6 +1,9 @@
-#1 Book Titles
+#3 Book Titles
 James and the Giant Peach
 Lord of the Rings
 The Color Purple
 The Graduate
 Fahrenheit 451
+
+# we both worked on our own but we talked together through each section.
+# we got stuck trying to remember how to put in a heading but we talked it out to figure it out.
--- challenge/pard_challenge.rb
+++ challenge/pard_challenge.rb
@@ -1,9 +1,12 @@
 # We choose 'paired_challenge' but typed it wrong as 'pard_challenge'
 librarary_location = "Denver"
 library_num_books = 5000
+# variable library_availabe is assigned to the boolean true
 library_availabe = true
+# variable library_best_seller is assigned to the string 'Harry Potter'
 library_best_seller = "Harry Potter"
+# variable library_series_avalable is assigned to the integer 7
 library_series_avalable = 7
-library_is_a_good_book = false
+library_is_a_good_book = true
 
 library_genres = ["Sci-fi", "Young Audls", "Fantasy", "History"]
