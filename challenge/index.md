7a65ce1226f1b789106a39feb91dbdff646f395e
Shirley DeCesari <shirley.decesari@gmail.com>
2022-03-01 19:40:04 -0800

    Initial commit

---
+++ challenge/index.md
@@ -0,0 +1,5 @@
+#1 Book Titles
+James and the Giant Peach
+Lord of the Rings
+Romeo and Juliet
+The Graduate
---
+++ challenge/pard_challenge.rb
@@ -0,0 +1,9 @@
+# We choose 'paired_challenge' but typed it wrong as 'pard_challenge'
+librarary_location = "Denver"
+library_num_books = 5000
+library_availabe = true
+library_best_seller = "Harry Potter"
+library_series_avalable = 7
+library_is_a_good_book = false
+
+library_genres = ["Sci-fi", "Young Audls", "Fantasy", "History"]
